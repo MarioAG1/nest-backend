bb48e0c79bf179995fc95e012bfed6cfeab883d0
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-09-13 12:03:20 +0200

    Generar un nuevo JWT, Asegurarse de que el usuario es quien dice ser y comprobar la validacion del token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { AuthService } from './auth.service';
 import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 
 
 @Controller('auth')
@@ -27,23 +29,36 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @Get()
   findAll(@Request() req: Request) {
-    const user = req["user"]
-    return user
+    // const user = req["user"]
+    // return user
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  //LoginResponse
+  @UseGuards(AuthGuard)
+  @Get("check-token")
+  checkToken(@Request() req: Request): LoginResponse {
+
+    const user = req["user"] as User;
+
+    return {
+      user,
+      token: this.authService.getJwToken({ id: user._id })
+    }
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
 }
